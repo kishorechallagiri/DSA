91b8d178fb52e207613f7537a172dcd1f47e31b5
kishorechallagiri <kishore.challagiri@gmail.com>
2026-06-24 13:56:59 +0530

    Time: 0 ms (100.00%) | Memory: 12.4 MB (51.27%) - LeetSync

--- 1293-three-consecutive-odds/three-consecutive-odds.py
+++ 1293-three-consecutive-odds/three-consecutive-odds.py
@@ -1,6 +1,6 @@
 class Solution(object):
     def threeConsecutiveOdds(self, arr):
-        count = 0
+        count=0
         for num in arr:
             if num % 2 != 0:
                 count += 1
@@ -8,5 +8,6 @@ class Solution(object):
                     return True
             else:
                 count = 0
+        return False  
 
-        return False
+
