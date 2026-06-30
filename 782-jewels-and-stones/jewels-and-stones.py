6ed8572583375f87042aab111ee54efafc9d1571
kishorechallagiri <kishore.challagiri@gmail.com>
2026-06-30 18:03:08 +0530

    Time: 0 ms (100.00%) | Memory: 12.3 MB (52.91%) - LeetSync

--- 782-jewels-and-stones/jewels-and-stones.py
+++ 782-jewels-and-stones/jewels-and-stones.py
@@ -1,13 +1,18 @@
 class Solution(object):
     def numJewelsInStones(self, jewels, stones):
         count=0
-        for i in range(len(stones)):
-            for j in range(len(jewels)):
-                if jewels[j]==stones[i]:
-                    count+=1
-                    break
-            
-        return count        
+        hash=set()
+        for i in range(len(jewels)):
+            hash.add(jewels[i])
+        for j in range(len(stones)) :
+            if stones[j] in hash:
+                count+=1   
+             
+              
+        return count
+
+
+   
 
 
 
