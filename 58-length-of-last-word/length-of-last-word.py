c3d102af0890d67940ba0716f5920d6acd28921e
kishorechallagiri <kishore.challagiri@gmail.com>
2026-06-30 12:39:03 +0530

    Time: 0 ms (100.00%) | Memory: 12.2 MB (97.86%) - LeetSync

--- 58-length-of-last-word/length-of-last-word.py
+++ 58-length-of-last-word/length-of-last-word.py
@@ -2,10 +2,10 @@ class Solution(object):
     def lengthOfLastWord(self, s):
         n=len(s)-1
         while(n>=0):
-            if s[n]==" ":
-                n-=1
-            else:
+            if s[n]!=" ":
+                
                 break
+            n-=1    
         count=0
         while(n>=0):
             if s[n]!=" ":
