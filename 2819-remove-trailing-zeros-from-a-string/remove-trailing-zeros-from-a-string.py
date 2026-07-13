d30cee0d3d2c948c1b1f9140849f13c280ac7992
kishorechallagiri <kishore.challagiri@gmail.com>
2026-07-13 18:43:52 +0530

    Time: 0 ms (100.00%) | Memory: 12.3 MB (82.32%) - LeetSync

---
+++ 2819-remove-trailing-zeros-from-a-string/remove-trailing-zeros-from-a-string.py
@@ -0,0 +1,7 @@
+class Solution(object):
+    def removeTrailingZeros(self, num):
+        i = len(num) - 1
+        while i >= 0 and num[i] == '0':
+           i-= 1
+        return num[:i + 1]
+        
