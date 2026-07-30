64c7be0108a32202ec045e362e0e23b213855b90
kishorechallagiri <kishore.challagiri@gmail.com>
2026-07-30 16:01:27 +0530

    Time: 4 ms (40.91%) | Memory: 12.6 MB (23.80%) - LeetSync

--- 496-next-greater-element-i/next-greater-element-i.py
+++ 496-next-greater-element-i/next-greater-element-i.py
@@ -3,27 +3,18 @@ class Solution(object):
         ngeMap={}
         stack=[]
         n=len(nums2)
-        stack.append(nums2[n-1])
-        ngeMap[nums2[n-1]]=-1
-        for i in range(n-2,-1,-1):
-            top=stack[-1]
-            if nums2[i]<top:
-                ngeMap[nums2[i]]=top
-            else:
-                while stack:
-                    if stack[-1]<nums2[i]:
-                        stack.pop()
-                    else:
-                        ngeMap[nums2[i]]=stack[-1]
-                        break
+        for i in range(n-1,-1,-1):
+                while stack and stack[-1]<nums2[i]:
+                    stack.pop()
                 if not stack:
-                    ngeMap[nums2[i]] = -1        
-            stack.append(nums2[i])    
+                    ngeMap[nums2[i]] = -1
+                else:
+                    ngeMap[nums2[i]] = stack[-1]        
+                stack.append(nums2[i])    
         ans=[]                    
         for i in range(len(nums1)):
             ans.append(ngeMap[nums1[i]])
             
-       
         return ans     
             
               
