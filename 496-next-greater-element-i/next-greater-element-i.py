a1714dd25095c8ceeca0087189c6b96e2665c0af
kishorechallagiri <kishore.challagiri@gmail.com>
2026-07-31 12:02:02 +0530

    Time: 0 ms (100.00%) | Memory: 12.6 MB (59.73%) - LeetSync

--- 496-next-greater-element-i/next-greater-element-i.py
+++ 496-next-greater-element-i/next-greater-element-i.py
@@ -1,21 +1,12 @@
 class Solution(object):
     def nextGreaterElement(self, nums1, nums2):
-        ngeMap={}
+        mp={}
         stack=[]
         n=len(nums2)
         for i in range(n-1,-1,-1):
-                while stack and stack[-1]<nums2[i]:
-                    stack.pop()
-                if not stack:
-                    ngeMap[nums2[i]] = -1
-                else:
-                    ngeMap[nums2[i]] = stack[-1]        
-                stack.append(nums2[i])    
-        ans=[]                    
-        for i in range(len(nums1)):
-            ans.append(ngeMap[nums1[i]])
-            
-        return ans     
-            
-              
+            while stack and stack[-1]<nums2[i]:
+                stack.pop()
+            mp[nums2[i]]=stack[-1] if stack else -1
+            stack.append(nums2[i])   
+        return [mp[x] for x in nums1]    
         
