b6e5e2a778fc0d0ff5c3848d8c7ccbc09d1dac7c
kishorechallagiri <kishore.challagiri@gmail.com>
2026-08-05 22:26:00 +0530

    Time: 0 ms (100.00%) | Memory: 12.9 MB (30.27%) - LeetSync

--- 35-search-insert-position/search-insert-position.py
+++ 35-search-insert-position/search-insert-position.py
@@ -7,11 +7,3 @@ class Solution(object):
             if nums[i]>target:
                 return i
         return len(nums)        
-
-
-k=Solution()
-nums=[1,3,5,6]
-target=5
-print(k.searchInsert(nums,target))               
-    
-        
