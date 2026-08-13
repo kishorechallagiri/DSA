c3061e75dbde468098c4ade1efaf59c94e95b68c
kishorechallagiri <kishore.challagiri@gmail.com>
2026-08-13 18:05:00 +0530

    Time: 5 ms (54.99%) | Memory: 12.5 MB (20.12%) - LeetSync

--- 4107-find-missing-elements/find-missing-elements.py
+++ 4107-find-missing-elements/find-missing-elements.py
@@ -1,15 +1,9 @@
 class Solution(object):
     def findMissingElements(self, nums):
-        nums.sort()
-        lst=[]
-        hash=set(nums)
-        first=nums[0]
-        last=nums[-1]
-        for i in range(first,last+1):
-            if i not in hash:
-                lst.append(i)
-            
-        return lst        
+        nums_set=set(nums)
+        first,last=min(nums),max(nums)
+        return [i for i in range(first,last+1) if i not in nums_set]
+       
 
 
         
