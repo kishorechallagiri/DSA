00049eb262978a7a644c4cb230df1f0a24ac9a09
kishorechallagiri <kishore.challagiri@gmail.com>
2026-08-20 18:28:56 +0530

    Time: 8 ms (8.22%) | Memory: 12.4 MB (54.10%) - LeetSync

--- 202-happy-number/happy-number.py
+++ 202-happy-number/happy-number.py
@@ -2,15 +2,11 @@ class Solution(object):
     def isHappy(self, n,hash=None):
         if hash is None:
             hash = set()
-
         if n == 1:
             return True
-
         if n in hash:
             return False
-
         hash.add(n)
-
         num = str(n)
         total = 0
         for ch in num:
