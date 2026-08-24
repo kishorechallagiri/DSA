d36c74a043e216987c1c57c95384a0ccdcef5d39
kishorechallagiri <kishore.challagiri@gmail.com>
2026-08-24 16:46:35 +0530

    Time: 4 ms (4.12%) | Memory: 12.7 MB (39.39%) - LeetSync

---
+++ 81-search-in-rotated-sorted-array-ii/search-in-rotated-sorted-array-ii.py
@@ -0,0 +1,7 @@
+class Solution(object):
+    def search(self, nums, target):
+        for num in nums:
+            if num==target:
+                return True
+        return False        
+        
