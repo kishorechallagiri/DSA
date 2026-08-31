389c051dc2188bc0c0b827a8f840937564cd2754
kishorechallagiri <kishore.challagiri@gmail.com>
2026-08-31 17:03:29 +0530

    Time: 483 ms (14.89%) | Memory: 12.4 MB (24.82%) - LeetSync

--- 4115-minimum-distance-between-three-equal-elements-i/minimum-distance-between-three-equal-elements-i.py
+++ 4115-minimum-distance-between-three-equal-elements-i/minimum-distance-between-three-equal-elements-i.py
@@ -1,13 +1,13 @@
 class Solution(object):
     def minimumDistance(self, nums):
-        ans = float('inf')
-
+        val=float('inf')
         for i in range(len(nums)):
-            for j in range(i + 1, len(nums)):
-                for k in range(j + 1, len(nums)):
-                    if nums[i] == nums[j] == nums[k]:
-                        distance = abs(i - j) + abs(j - k) + abs(k - i)
-                        ans = min(ans, distance)
+            for j in range(i+1,len(nums)):
+                for k in range(j+1,len(nums)):
+                    if nums[i]==nums[j]==nums[k]:
+                        ans= abs(i - j) + abs(j - k) + abs(k - i)
+                        val=min(ans,val)
 
-        return ans if ans != float('inf') else -1
-            
+        return val  if val!=float('inf')  else -1         
+
+        
